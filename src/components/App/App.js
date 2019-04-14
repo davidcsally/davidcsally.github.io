874ade1bcad25c34ef2d1ce82d4f666375410eb1
David Sally <davidcsally@gmail.com>
2019-04-13 22:14:17 -0700

    Remove resize listener

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import throttle from 'lodash.throttle';
 
 import * as S from './styles';
 
@@ -10,31 +9,10 @@ import Angellist from '../icons/Angellist/Angellist';
 import Medium from '../icons/Medium/Medium';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = { paddingTop: '200px' };
-    this.throttledPaddingTop = throttle(this.paddingTop, 100);
-  }
-
-  componentDidMount() {
-    window.addEventListener('resize', throttle(this.throttledPaddingTop));
-    this.throttledPaddingTop();
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.throttledPaddingTop);
-  }
-
-  /** keep the text in the  .~* ✨ stars 💫 *~.  */
-  paddingTop = () => {
-    const height = document.documentElement.clientWidth * 0.2;
-    this.setState({ paddingTop: (height < 200) ? `${height}px` : `200px` });
-  }
-
   render() {
     return (
       <S.Container backgroundImage={heroImage}>
-        <S.TextContainer paddingTop={this.state.paddingTop}>
+        <S.TextContainer paddingTop={'100px'}>
           <S.P>hello, world!</S.P>
           <S.H1Container>
             <S.H1>David Sally</S.H1>
