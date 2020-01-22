fa2687170d39c3ce1d04765467c30feac5d00d7a
David Sally <davidcsally@gmail.com>
2020-01-21 21:44:10 -0800

    Updates

---
+++ precache-manifest.57aff34e403b202f68c07252b005440e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4347a2885b359695763928aeaf1a7765",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f1e3f4cf0e370b60b83",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "131df27ae3db6f988ce1",
+    "url": "/static/css/main.056b9216.chunk.css"
+  },
+  {
+    "revision": "9f1e3f4cf0e370b60b83",
+    "url": "/static/js/2.d82bcbcd.chunk.js"
+  },
+  {
+    "revision": "06f524020835665475d48144db8f9294",
+    "url": "/static/js/2.d82bcbcd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "131df27ae3db6f988ce1",
+    "url": "/static/js/main.3b04290a.chunk.js"
+  },
+  {
+    "revision": "d89a56c075c236875916",
+    "url": "/static/js/runtime-main.f7069b67.js"
+  },
+  {
+    "revision": "1e1852b53bfb4e7c89a16cec828e9a8d",
+    "url": "/static/media/Futura-Bold.1e1852b5.woff"
+  },
+  {
+    "revision": "b556299d84ccef97308de5bea0aa4502",
+    "url": "/static/media/Futura-Light.b556299d.woff"
+  },
+  {
+    "revision": "f2c2be7d44c1d3faafb995acf98bb21e",
+    "url": "/static/media/Futura-Medium.f2c2be7d.woff"
+  },
+  {
+    "revision": "6fe888f21ceaf0b3c8ec4d06a4445f23",
+    "url": "/static/media/pixel.6fe888f2.cur"
+  },
+  {
+    "revision": "b34c1adefdbb33c9ccf61e770636af7a",
+    "url": "/static/media/yosemite.b34c1ade.jpg"
+  }
+]);
