030c9133c457f439e6d568f29fbdb82a301aa653
David Sally <davidcsally@gmail.com>
2020-01-22 16:05:33 -0800

    Add border-box.css package to apply box-sizing to all elements

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,14 +1,6 @@
 import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
-  html {
-    box-sizing: border-box;
-  }
-
-  *, *:before, *:after {
-    box-sizing: inherit;
-  }
-
   body {
     font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
       "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
