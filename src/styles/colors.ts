80e4ba352cc2ffafbf7910b70927e1fa6feff590
David Sally <davidcsally@gmail.com>
2020-02-10 22:57:24 -0800

    Create color palette file in styled-components
    This will be used to set colors using css variables.

---
+++ src/styles/colors.ts
@@ -0,0 +1,11 @@
+import { css } from 'styled-components'
+
+export const palette = css`
+  :root {
+    --outline-color: #3500D3;
+
+    --chat-primary: #0A5BFF;
+
+    --system-red: #bc0f0b;
+  }
+`
