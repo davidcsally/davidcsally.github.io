72eeed72bb63a962340746d8bf9e00555243dc1f
David Sally <davidcsally@gmail.com>
2020-02-16 22:15:44 -0800

    Fix and rename App test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,8 @@
+/* eslint-env jest */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from 'components/App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
