43bc8ae6b9cfc83166bb1c271e38e86322d5770d
David Sally <davidcsally@gmail.com>
2020-04-11 01:50:22 -0700

    Fix a bunch of small design stuff + lint everything

--- src/components/PortfolioBlock/index.tsx
+++ src/components/PortfolioBlock/index.tsx
@@ -3,16 +3,16 @@ import styled from 'styled-components';
 
 const H2 = styled.h2`
   color: #ffffff;
-`
+`;
 
 const H3 = styled.h3`
   color: #ffffff;
-`
+`;
 
 const P = styled.p`
   line-height: 1.5rem;
   color: #ffffff;
-`
+`;
 
 const List = styled.ul`
   list-style: none;
@@ -24,7 +24,7 @@ const List = styled.ul`
   justify-content: center;
 
   transition: all 200ms linear;
-`
+`;
 
 const Box = styled.li`
   display: flex;
@@ -45,7 +45,7 @@ const Box = styled.li`
   &:hover {
     transform: scale(1.05);
   }
-`
+`;
 
 const Container = styled.div`
   min-height: 500px;
@@ -55,70 +55,82 @@ const Container = styled.div`
   justify-content: flex-start;
   align-items: center;
   background-color: #000000;
-`
+`;
 
-const PortfolioBlock = () => {
-  return (
-    <Container>
-      <H2>Projects</H2>
-      <List>
-        <Box>
-          <H3>MeUndies</H3>
-          <P>
-            At MeUndies, I helped build out new features and redesign existing ones.
-            Some of the pages and features I played a major role in: <br/>
-              - Desktop navigation <br/>
-              - Checkout <br/>
-              - The gift card flow <br/>
-              - Account Management <br/>
-            <br/>
-            In addition, I helped build our component library, and many of the shared 
-            components and icons used on the site.
-          </P>
-        </Box>
-        <Box>
-          <H3>Disney</H3>
-          <P>
-            At Disney, I worked on www.shopdisney.com, which consisted of two main 
-            projects: the catalog, and checkout. <br/> <br/>
-            
-            The catalog portion consisted of an es5 backbone application, that was 
-            managed with a propetary CMS.<br/><br/>
+const PortfolioBlock = () => (
+  <Container>
+    <H2>Projects</H2>
+    <List>
+      <Box>
+        <H3>MeUndies</H3>
+        <P>
+          At MeUndies, I helped build out new features and redesign existing ones.
+          Some of the pages and features I played a major role in:
+          <br />
+          - Desktop navigation
+          <br />
+          - Checkout
+          <br />
+          - The gift card flow
+          <br />
+          - Account Management
+          <br />
+          <br />
+          In addition, I helped build our component library, and many of the shared
+          components and icons used on the site.
+        </P>
+      </Box>
+      <Box>
+        <H3>Disney</H3>
+        <P>
+          At Disney, I worked on www.shopdisney.com, which consisted of two main
+          projects: the catalog, and checkout.
+          {' '}
+          <br />
+          {' '}
+          <br />
 
-            Checkout (and the account portion) were built using create-react-app, 
-            was a separate, PCI compliant single page application.
-          </P>
-        </Box>
-        <Box>
-          <H3>MedCircle</H3>
-          <P>
-            At MedCircle, I worked on our full stack health focused social network. 
-            <br/><br/>
-            Our techstack included a server rendered React + Redux front end, with 
-            a node based API. 
-            <br/><br/>
-          </P>
-        </Box>
-        <Box>
-          <H3>React-Sight</H3>
-          <P>
-            React-Sight is an open source Chrome extension for React developers, and 
-            provides them with a visual, graph based version of the React DevTools. 
-            <br/><br/>
-            React-Sight has over 2,000 stars on GitHub, and over 11,000 downloads from 
-            the Chrome extensions marketplace. 
-          </P>
-        </Box>
-        <Box>
-          <H3>Bio-Rad Laboratories</H3>
-          <P>
-            At Bio-Rad, I was involved in prototyping, building, and testing next generation 
-            droplet digital PCR technology.
-          </P>
-        </Box>
-      </List>
-    </Container>
-  )
-}
+          The catalog portion consisted of an es5 backbone application, that was
+          managed with a propetary CMS.
+          <br />
+          <br />
 
-export default PortfolioBlock
+          Checkout (and the account portion) were built using create-react-app,
+          was a separate, PCI compliant single page application.
+        </P>
+      </Box>
+      <Box>
+        <H3>MedCircle</H3>
+        <P>
+          At MedCircle, I worked on our full stack health focused social network.
+          <br />
+          <br />
+          Our techstack included a server rendered React + Redux front end, with
+          a node based API.
+          <br />
+          <br />
+        </P>
+      </Box>
+      <Box>
+        <H3>React-Sight</H3>
+        <P>
+          React-Sight is an open source Chrome extension for React developers, and
+          provides them with a visual, graph based version of the React DevTools.
+          <br />
+          <br />
+          React-Sight has over 2,000 stars on GitHub, and over 11,000 downloads from
+          the Chrome extensions marketplace.
+        </P>
+      </Box>
+      <Box>
+        <H3>Bio-Rad Laboratories</H3>
+        <P>
+          At Bio-Rad, I was involved in prototyping, building, and testing next generation
+          droplet digital PCR technology.
+        </P>
+      </Box>
+    </List>
+  </Container>
+);
+
+export default PortfolioBlock;
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { hydrate, render } from "react-dom";
+import { hydrate, render } from 'react-dom';
 import 'normalize.css';
 import 'border-box.css';
 
@@ -8,6 +8,6 @@ import { printAscii } from './utils';
 
 printAscii();
 
-const rootElement = document.getElementById("groot")!;
+const rootElement = document.getElementById('groot')!;
 if (rootElement.hasChildNodes()) hydrate(<App />, rootElement);
 else render(<App />, rootElement);
