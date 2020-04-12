26ba9f32d0718d58028544bc23b8058081e47835
David Sally <davidcsally@gmail.com>
2020-04-12 01:03:37 -0700

    WIP update and turn on PortfolioBlock

---
+++ src/components/Blocks/index.tsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+import { flex } from 'styles/mixins';
+import { media } from 'styles/media';
+
+export const Column = styled.div`
+  ${flex({ direction: 'column' })}
+  width: 100%;
+`;
+
+export const Row = styled.div`
+  ${flex({ direction: 'row' })}
+  width: 100%;
+`;
+
+export const ResponsiveCol = styled.div`
+  ${flex({ direction: 'column' })}
+  width: 100%;
+
+  ${media.tablet`
+    flex-direction: row;
+  `}
+`;
