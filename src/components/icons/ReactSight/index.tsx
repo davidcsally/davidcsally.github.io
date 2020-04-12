cefb81e211f37a2263cfbda0b7366fd791f2965c
David Sally <davidcsally@gmail.com>
2020-04-12 01:02:40 -0700

    WIP make some new logo icons

---
+++ src/components/icons/ReactSight/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import logo from './logo.png';
+
+const Img = styled.img`
+  @keyframes bounce {
+    0% {
+      transform: translateY(0px);
+    }
+    50% {
+      transform: translateY(18px);
+    }
+    100% {
+      transform: translateY(-2px);
+    }
+  }
+
+  width: 60%;
+  height: auto;
+  animation: 1500ms bounce infinite;
+  animation-play-state: paused;
+
+  &:hover {
+    animation-play-state: running;
+  }
+`;
+
+export const ReactSight: React.FC<any> = ({ className }) => (
+  <Img className={className} src={logo} alt="Bio-Rad Laboratories" />
+);
