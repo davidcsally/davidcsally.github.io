1739b780a0289f007635ce78a1d0fe7d76ebf5cd
David Sally <davidcsally@gmail.com>
2021-02-14 14:21:54 -0800

    Update CookieBar so it doesn't trigger LCP or get flagged by ublock

--- src/components/CookieBar/index.tsx
+++ src/components/CookieBar/index.tsx
@@ -17,9 +17,10 @@ const Row = styled.div`
   ${flex()}
 `
 
-const Container = styled(motion.aside)`
+const Container = styled(motion.section)`
   ${flex()}
   position: fixed;
+  display: inline-block;
   bottom: 0;
   min-height: 5rem;
   background-color: white;
@@ -63,6 +64,10 @@ const Button = styled.button`
   cursor: pointer;
 `
 
+const InlineBlock = styled.p`
+  display: inline-block;
+`
+
 export const CookieBar: React.FC<Props> = ({ isCookieBarOpen, closeCookieBar }) => (
   <AnimatePresence>
     {isCookieBarOpen
@@ -70,7 +75,7 @@ export const CookieBar: React.FC<Props> = ({ isCookieBarOpen, closeCookieBar }) 
         <Container key="cookie-bar" {...animation}>
           <Column>
             <Row>
-              <p>This site uses cookies. Or does it?</p>
+              <InlineBlock>This site uses cookies. Or does it?</InlineBlock>
               <Button
                 type="button"
                 onClick={() => closeCookieBar()}
