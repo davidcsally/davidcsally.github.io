ec195577457066cc52a78c25f878b4584377b8c1
David Sally <davidcsally@gmail.com>
2021-08-17 00:08:52 -0700

    Create test for PortfolioBlock

---
+++ src/components/modules/PortfolioBlock/index.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react'
+
+import { PortfolioBlock } from '.'
+
+describe('PortfolioBlock', () => {
+  it('renders without crashing', () => {
+    render(<PortfolioBlock />)
+
+    expect(screen.getByTestId('portfolio-module')).toBeVisible()
+  })
+})
