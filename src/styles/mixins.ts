021cbf50586f24088194ea6d2574957e87ac2117
David Sally <davidcsally@gmail.com>
2021-08-23 16:06:46 -0700

    Cleanup some css pertaining to text and layout
    There's still a lot I need to cleanup, but this is a good start 😀.

--- src/styles/mixins.ts
+++ src/styles/mixins.ts
@@ -3,6 +3,7 @@ import { css } from 'styled-components'
 interface Flex {
   direction?: 'column' | 'row'
   alignItems?: 'center' | 'flex-start' | 'flex-end'
+  flexWrap?: 'wrap' | 'nowrap'
   justifyContent?:
     | 'center'
     | 'flex-start'
@@ -17,9 +18,11 @@ export const flex = ({
   direction = 'row',
   alignItems = 'center',
   justifyContent = 'center',
+  flexWrap = 'nowrap',
 }: Flex = {}) => css`
   display: flex;
   flex-direction: ${direction};
   justify-content: ${justifyContent};
   align-items: ${alignItems};
+  flex-wrap: ${flexWrap};
 `
